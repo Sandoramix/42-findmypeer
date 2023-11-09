0d7afc31853edeeb0679aa54edc929733eb9f518
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-11-09 20:26:48 +0100

    [refactor] class to attribute && added my link

--- public/js/global-variables.js
+++ public/js/global-variables.js
@@ -1,8 +1,8 @@
 const SEARCH_INPUT = document.getElementById(`search`);
 const FORM = document.getElementById(`search-form`);
 const LOADING_SPINNER = document.getElementById(`loading`);
-const REFETCH_CNTS = document.querySelectorAll(`.refetch-cnt`);
-const REFETCH_TIMES = document.querySelectorAll(`.refetch-time`);
+const REFETCH_CNTS = document.querySelectorAll(`[refetch-cnt]`);
+const REFETCH_TIMES = document.querySelectorAll(`[refetch-time]`);
 const TABS_BUTTONS = document.querySelectorAll(`[data-selectedtab]`);
 
 const CLUSTER = {

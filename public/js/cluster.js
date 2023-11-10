83affb9d6bd677ba00b5caacc3d6d46ac9c65b3b
Oleksandr Dudniak <odudniak@c2r1p1.42firenze.it>
2023-11-10 10:33:27 +0100

    simplified layout && started implementating the autogeneration of clusters

--- public/js/cluster.js
+++ public/js/cluster.js
@@ -10,8 +10,8 @@ function updateCluster(){
 				const [cluster, row, pc] = [clusterIdx + 1, rows.length - cRowIdx, crPcIdx + 1];
 				const user = findUserByPosition(cluster, row, pc);
 				const userLink = crPc.querySelector(`a`);
+				const usernameP = userLink.querySelector(`[data-username]`);
 
-				userLink.textContent = `n/a`;
 				userLink.href = ``
 
 				userLink.classList.toggle(`text-neutral-600`, false);
@@ -25,15 +25,59 @@ function updateCluster(){
 				if (!user){
 					userLink.classList.add('disabled');
 					userLink.classList.toggle(`text-neutral-600`, true);
+
+					usernameP.textContent = `n/a`;
 					return;
 				}
 				userLink.classList.remove('disabled');
-				userLink.textContent = user.username;
 				userLink.href = `https://profile.intra.42.fr/users/${user.username}`
 				userLink.target = '_black';
 				userLink.referrerPolicy = 'noreferrer'
+
+				usernameP.textContent = user.username;
 
 			})
 		})
 	})
 }
+
+
+
+function generateCluster(){
+	CLUSTER_TABLES.forEach((cluster, clusterIdx)=>{
+		const rows = cluster.querySelectorAll(`tbody tr`);
+		rows.forEach((cRow, cRowIdx)=>{
+			const pcs = cRow.querySelectorAll(`.pc`);
+			pcs.forEach((crPc, crPcIdx)=>{
+				// const [cluster, row, pc] = [clusterIdx + 1, rows.length - cRowIdx, crPcIdx + 1];
+				// const user = findUserByPosition(cluster, row, pc);
+				// const userLink = document.createElement(`a`);
+
+				// userLink.textContent = `n/a`;
+				// userLink.href = ``
+
+				// userLink.classList.toggle(`text-neutral-600`, false);
+				// userLink.classList.toggle(`!text-amber-400`, false);
+				// // userLink.classList.toggle(`!font-medium`, false);
+				// if (user && isSearchIncluded(user.username) || isSearchIncluded(`c${cluster}r${row}p${pc}`)){
+				// 	userLink.classList.toggle(`!text-amber-400`, true);
+				// 	// userLink.classList.toggle(`!font-medium`, true);
+				// }
+
+				// if (!user){
+				// 	userLink.classList.add('disabled');
+				// 	userLink.classList.toggle(`text-neutral-600`, true);
+				// 	return;
+				// }
+				// userLink.classList.remove('disabled');
+				// userLink.textContent = user.username;
+				// userLink.href = `https://profile.intra.42.fr/users/${user.username}`
+				// userLink.target = '_black';
+				// userLink.referrerPolicy = 'noreferrer'
+
+			})
+		})
+	})
+}
+
+generateCluster();

020061b26b19c2cf46a329a8a15af724c705ae16
Oleksandr Dudniak <odudniak@student.42firenze.it>
2023-11-19 11:52:21 +0100

    [update] loading clusters from backend

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ function updateElements() {
 
 SEARCH_INPUT.addEventListener('keyup', (ev) => {
 	clearTimeout(searchDebounceTimeout);
-	searchDebounceTimeout = setTimeout(updateElements, 150);
+	searchDebounceTimeout = setTimeout(updateElements, 100);
 });
 FORM.addEventListener(`submit`, (ev) => {
 	ev.preventDefault();
@@ -13,21 +13,46 @@ FORM.addEventListener(`submit`, (ev) => {
 	updateElements();
 });
 
+
 function fetchPeers() {
 	// TODO REPLACE TO: document.location.origin
 	updateLoading(true);
-	fetch(`${document.location.origin}/peers`, { method: 'GET' }).then(res => res.json()).then(data => {
-		const { users, refreshAt } = data;
-		usersData = users;
+	fetch(`${document.location.origin}/peers`, { method: 'GET' })
+		.then(res => res.json())
+		.then(data => {
+			const { users, refreshAt } = data;
+			PEERS = users;
 
-		updateLoading(false);
-		updateRefetchTime(refreshAt);
-		updateElements();
-		const now = Date.now();
-		const nowD = new Date();
-		setTimeout(fetchPeers, refreshAt - now >= 0 ? refreshAt - now : (1000 * 60) - (nowD.getSeconds() * 1000));
-	});
+			updateLoading(false);
+			updateRefetchTime(refreshAt);
+			updateElements();
+			const now = Date.now();
+			const nowD = new Date();
+			setTimeout(fetchPeers, refreshAt - now >= 0 ? refreshAt - now : (1000 * 60) - (nowD.getSeconds() * 1000));
+		});
 }
-fetchPeers();
 
-
+function fetchClusters() {
+	updateLoading(true);
+	fetch(`${document.location.origin}/clusters`, { method: 'GET' })
+		.then(res => res.json())
+		.then(data => {
+			CLUSTERS = data ?? [];
+			if (CLUSTERS.length == 0) {
+				showMessage("No cluster has been configured yet");
+				updateLoading(false);
+				return;
+			}
+			CLUSTER_TABLES = [];
+			CLUSTERS.forEach(c => generateCluster(c, CLUSTERS_SECTION));
+			fetchPeers();
+		})
+		.catch(err => {
+			if (FETCH_RETRIES.clusters++ < MAX_RETRIES) {
+				return fetchClusters();
+			}
+			return showMessage("Server is not reachable, try again later", true);
+		});
+}
+fetchClusters();
+updateElements();

a85b71cff2b6f956fab392f0fae2678ef2859ddc
Oleksandr Dudniak <odudniak@student.42firenze.it>
2023-11-19 11:52:49 +0100

    added docs

--- public/js/global-variables.js
+++ public/js/global-variables.js
@@ -1,15 +1,39 @@
+/**
+ * @typedef {Object} ClusterPcColorConfig
+ * @property {string} default
+ * @property {string} matched
+
+ * @typedef {Object} ClusterPcColorsConfig
+ * @property {...ClusterPcColorConfig} active
+ * @property {...ClusterPcColorConfig} inactive
+
+ * @typedef {Object} ClusterConfig
+ * @property {number} id
+ * @property {string} name
+ * @property {number} rows
+ * @property {number} columns
+ * @property {number[]} spacerColumns
+ * @property {...ClusterPcColorsConfig} pcColors
+ * @property {boolean} isWeird
+ * @property {null | Object} rotations
+ */
+
 const SEARCH_INPUT = document.getElementById(`search`);
 const FORM = document.getElementById(`search-form`);
 const LOADING_SPINNER = document.getElementById(`loading`);
 const REFETCH_CNTS = document.querySelectorAll(`[data-refetch-cnt]`);
 const REFETCH_TIMES = document.querySelectorAll(`[data-refetch-time]`);
 const TABS_BUTTONS = document.querySelectorAll(`[data-selectedtab]`);
+
+// TEMPLATES
 const PC_TEMPLATE = document.querySelector(`[data-template="pc"]`);
 
+// FETCH MAX RETRIES
+const FETCH_RETRIES = { peers: 0, clusters: 0 };
 const MAX_RETRIES = 3;
 
 /**
- * @type {any[]}
+ * @type {...ClusterConfig[]}
  */
 var CLUSTERS;
 
@@ -22,6 +46,6 @@ var PEERS = [];
 var refetchTimeout;
 
 
-function searchValue(){
-	return SEARCH_INPUT.value
+function searchValue() {
+	return SEARCH_INPUT.value;
 }
